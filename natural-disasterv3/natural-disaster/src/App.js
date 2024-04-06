b8487cb1ebcecc8fb3232516f8e0326d34ec9161
mmdavid1 <96997314+mmdavid1@users.noreply.github.com>
2024-04-06 16:07:49 -0700

    added ability to click on and zoom into individual states

--- natural-disasterv3/natural-disaster/src/App.js
+++ natural-disasterv3/natural-disaster/src/App.js
@@ -1,14 +1,69 @@
 import React from "react";
-import { MapContainer, TileLayer, Polygon } from "react-leaflet";
+import { MapContainer, TileLayer, Polygon, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { statesData } from "./data";
 import "./App.css";
+import L from 'leaflet';
 
 const center = [40.63463151377654, -97.89969605983609];
 const maxBounds = [
-  [24.396308, -125.0], // Southwest coordinates (latitude, longitude)
-  [49.384358, -66.93457] // Northeast coordinates (latitude, longitude)
+  [24.396308, -125.0],
+  [49.384358, -66.93457] 
 ];
+
+function StatePolygon({ state }) {
+
+  const map = useMap();
+  const coordinates = state.geometry.coordinates[0].map((item) => [
+    item[1],
+    item[0],
+  ]);
+
+  const handleClick = () => {
+    const bounds = L.latLngBounds(coordinates);
+    map.fitBounds(bounds);
+  };
+
+  return (
+    <Polygon
+      pathOptions={{
+        fillOpacity: 0.7,
+        opacity: 1,
+        color: "white",
+        fillColor: "#007bff",
+        weight: 2,
+        dashArray: 3,
+      }}
+      positions={coordinates}
+      eventHandlers={{
+        mouseover: (e) => {
+          const layer = e.target;
+          layer.setStyle({
+            dashArray: "",
+            opacity: 1,
+            color: "white",
+            fillColor: "#808080",
+            weight: 2,
+            fillOpacity: 0.7,
+          });
+        },
+        mouseout: (e) => {
+          const layer = e.target;
+          layer.setStyle({
+            weight: 2,
+            dashArray: "3",
+            color: "white",
+            fillColor: "#007bff",
+            fillOpacity: 0.7,
+          });
+        },
+        click: handleClick,
+      }}
+    />
+  );
+}
+
+
 export default function App() {
   return (
     <MapContainer
@@ -22,50 +77,9 @@ export default function App() {
         url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=SBZhTEOwJUIIKTs8PQRL"
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
       />
-      {statesData.features.map((state) => {
-        const coordinates = state.geometry.coordinates[0].map((item) => [
-          item[1],
-          item[0],
-        ]);
-
-        return (
-          <Polygon
-            pathOptions={{
-              fillOpacity: 0.7,
-              opacity: 1,
-              color: "white",
-              fillColor: "#007bff",
-              weight: 2,
-              dashArray: 3,
-            }}
-            positions={coordinates}
-            eventHandlers={{
-              mouseover: (e) => {
-                const layer = e.target;
-                layer.setStyle({
-                  dashArray: "",
-                  opacity: 1,
-                  color: "white",
-                  fillColor: "#808080",
-                  weight: 2,
-                  fillOpacity: 0.7,
-                });
-              },
-              mouseout: (e) => {
-                const layer = e.target;
-                layer.setStyle({
-                  weight: 2,
-                  dashArray: "3",
-                  color: "white",
-                  fillColor: "#007bff",
-                  fillOpacity: 0.7,
-                });
-              },
-              click: (e) => {},
-            }}
-          />
-        );
-      })}
+      {statesData.features.map((state, index) => (
+        <StatePolygon key={index} state={state} />
+      ))}
     </MapContainer>
   );
 }
