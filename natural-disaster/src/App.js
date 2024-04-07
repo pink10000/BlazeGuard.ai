8b526ffab9691dc5b091b24cc848f85a00b38dd7
Cody Rupp <crupp@ucsd.edu>
2024-04-06 19:35:52 -0700

    added button and default fire icons

--- natural-disaster/src/App.js
+++ natural-disaster/src/App.js
@@ -1,15 +1,64 @@
 import React, { useState } from "react";
-import { MapContainer, TileLayer, Polygon, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, Polygon, useMap, Marker, Popup} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { statesData } from "./data";
 import "./App.css";
 import L from "leaflet";
+import fireIcon from './img/fire-icon.png'
 
 const center = [40.63463151377654, -97.89969605983609];
 const maxBounds = [
   [24.396308, -125.0],
   [49.384358, -66.93457],
 ];
+
+// Sample wildfire data
+const wildfireData = [
+  { position: [41.634, -98.899], name: "Wildfire 1" },
+  { position: [39.634, -96.899], name: "Wildfire 2" },
+  // Add more wildfire data as needed
+];
+
+function WildfireLayer() {
+  const fireMarkerIcon = L.icon({
+    iconUrl: fireIcon,
+    iconSize: [32, 32], // Adjust the size of the icon as needed
+    iconAnchor: [16, 32], // Position the icon's anchor point
+    popupAnchor: [0, -32] // Position the popup relative to the icon
+  });
+
+  return (
+    <>
+      {wildfireData.map((wildfire, index) => (
+        <Marker key={index} position={wildfire.position} icon={fireMarkerIcon}>
+          <Popup>{wildfire.name}</Popup>
+        </Marker>
+      ))}
+    </>
+  );
+}
+
+function Button() {
+  const helloPopup = L.popup().setContent('Hello World!');
+  const map = useMap();
+
+  const handleClick = () => {
+    helloPopup.setLatLng(map.getCenter()).openOn(map);
+  };
+
+  return (
+    <button className="leaflet-bar leaflet-control leaflet-control-custom" onClick={handleClick} style={{
+      position: "absolute",
+      text: "hi",
+      top: "20px", // Adjust top position
+      right: "20px", // Adjust right position
+      zIndex: 1000, // Ensure the button appears above other elements on the map
+      fontSize: "20px", padding: "10px 20px"}}>
+      <i className="fa fa-globe"></i>
+      Button
+    </button>
+  );
+}
 
 function StatePolygon({ state, selected, onClick }) {
   const map = useMap();
@@ -78,6 +127,10 @@ export default function App() {
         url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=SBZhTEOwJUIIKTs8PQRL"
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
       />
+
+      <WildfireLayer /> {/* Render the WildfireLayer component */}
+
+      <Button />
       {statesData.features.map((state, index) => (
         <StatePolygon
           key={index}
